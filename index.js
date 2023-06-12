037cb6f70c219f449a70ffa9069c20c43885e79e
duan spot <brandennjonge@gmail.com>
2023-06-12 14:01:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -7,29 +7,20 @@
 
 
 
-// function addingEventListener(){
+function addingEventListener(){
 
-//     const input=document.getElementById("input")
-//     input.addEventListener('event',function(){
+    const input=document.getElementById("button")
+    input.addEventListener('event',function(){
     
     
-//         alert('clickme')
-//     })
+        alert('clickme')
+    })
 
-// // input=document.getElementById('button',addEventListener)
+// input=document.getElementById('button',addEventListener)
     
-// }
-
-// addingEventListener();
-
-const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert);
-
+addingEventListener();
 
 
 
